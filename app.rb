372cd4a568acf0e73633ad0c668b3642447724b6
Jacqueline Lee <anginblu@gmail.com>
2018-01-04 22:57:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,11 +17,12 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase_array = []
-    @phrase_array << params[:phrase].to_s
+    @count = 1
+    @phrase = params[:phrase].to_s
     until @number == @count
       "#{@phrase}"
       count += 1
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
