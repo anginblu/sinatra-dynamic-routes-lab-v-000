b3509c931e5ef73477d1643d16be3bb748736a0b
Jacqueline Lee <anginblu@gmail.com>
2018-01-04 22:48:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
     @name = params[:name].to_s
-
+    "#{@name.reverse}."
 
   end
 
   get '/square/:number' do
     @number = params[:number].to_i
-    
+
   end
 
   get '/say/:number/:phrase' do
